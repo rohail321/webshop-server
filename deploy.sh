26c13c335a3bcfc5c6046c17727c3b88bb162b25
rohail321 <qrohail32@gmail.com>
2023-01-08 06:55:38 +0000

    final

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,33 @@
-gcloud_list(){
-    local gcloud="$(gcloud app versions list)"
-    local array=()
-    for word in $gcloud
-    do 
-        array+=($word)
-    done
-    echo "${array[@]}"
+ #! /bin/bash -x
+
+echo "Deploying eShop/server to google cloud \n"
+
+# Returns a list of all gcloud deployment versions
+gcloud_list() {
+  local gcloud="$(gcloud app versions list)"
+  local array=()
+  for word in $gcloud
+  do  
+    array+=($word)
+  done
+  echo "${array[@]}"
 }
 
-
-delete_version(){
+# Deletes unnecessary deployments that are not utilized
+delete_version() {
     local array=($@)
-    for ((i=0; i< ${#array[@]}; ++i)); do
-        if [[${array[$i]}==*"0.00"*]]; then
-            local version=${array[$i-1]}
-            local command="gcloud -q app version delete $version"
+    for ((i = 0; i < ${#array[@]}; ++i)); do
+        if [[ ${array[$i]} == *"0.00"* ]]; then
+            local version=${array[$i - 1]}
+            local command="gcloud -q app versions delete $version"
             eval $command
         fi
     done
 }
 
-local list =$(gcloud_list)
+# Ecxecutes functions
+list=$(gcloud_list)
 delete_version $list
 
-gcloud  app deploy --stop-previous-version
+# deploy to google cloud provider
+gcloud app deploy --stop-previous-version
